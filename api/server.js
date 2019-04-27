4606b5546c22351fce9e2381b73b119bc003aae0
ridbay <balogunridwan@gmail.com>
2019-04-27 08:05:14 +0100

    Add API routes

--- api/server.js
+++ api/server.js
@@ -1,58 +1,10 @@
-import bodyParser from 'body-parser';
-import express from 'express';
+const server = require('./server/config/app')();
+const config = require('./server/config/env_config/config');
+const db = require('./server/config/db');
 
-// import from User to define the endpoints 
-import User from './src/controllers/User';
-import Account from './src/controllers/Account';
-import Transaction from './src/models/Transaction';
+// create the basic server setup
 
+server.create(config, db);
 
-// create an instance of express
-const app = express();
-
-// Set port
-const port = process.env.PORT || 8000;
-
-// Configure app to user bodyParser
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-// // Setting up the middleware needed to get access to the request body
-// app.use(express.json());
-
-
-// An endpoint/route to test if the server is working
-app.get('/', (req, res) => {
-  return res.status(200).send({'message': 'Hi Ridwan, Your server is working'});
-});
-
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
-
-//user end points
-
-app.post('/api/v1/users', User.create);
-app.get('/api/v1/users', User.getAll);
-app.get('/api/v1/users/:id', User.getOne);
-app.put('/api/v1/users/:id', User.update);
-app.delete('/api/v1/users/:id', User.delete);
-
-//accounts end points
-
-app.post('/api/v1/accounts', Account.create);
-app.get('/api/v1/accounts', Account.getAll);
-app.get('/api/v1/accounts/:id', Account.getOne);
-app.patch('/api/v1/accounts/:id', Account.update);
-app.delete('/api/v1/accounts/:id', Account.delete);
-
-//transactions end points
-
-app.post('/api/v1/transactions/:id/debit', Transaction.create); //Debit a bank account.
-app.post('/api/v1/transactions/:id/credit', Transaction.create); //Credit a bank account.
-// app.get('/api/v1/transactions', Transaction.getAll);
-// app.get('/api/v1/transactions/:id', Transaction.getOne);
-app.put('/api/v1/transactions/:id', Transaction.update);
-app.delete('/api/v1/transactions/:id', Transaction.delete);
-
-// // Export app for testing purposes
-export default app;
+// start the server
+server.start();
